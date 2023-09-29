b23e00ef71a3145f7b28bc0d4a13c3d62a9091d7
JBLGithub <jordi.laird@gmail.com>
2023-09-29 18:36:42 +0100

    fix css

--- frontend/src/components/aboutme/AboutMe.tsx
+++ frontend/src/components/aboutme/AboutMe.tsx
@@ -9,25 +9,32 @@ const AboutMe = () => {
                 I am consistently seeking opportunities for knowledge enhancement through collaborative efforts in both professional work environments, academic pursuits, and personal projects.
                 Some of my activities include skiing, sailing, scuba diving, golf, frisby golf, cycling and when I'm not in the wilderness, I'm learning about networking and security.</p>
             <h2>Education</h2>
-            <div className={styles.educontainer}>
-                <p>Msci Computer Science (2024) - University of St Andrews</p>
-                <p>SQA Highers/Adv Highers (2016/18) - High School of Dundee</p>
-                <p>Collège/Lycée (2011/16) - Lycée Piere de Coubertin</p>
+            <div className={styles.container}>
+                <p className={styles.p1}>University of St Andrews (2024)</p>
+                <p className={styles.p2}>Msci Computer Science</p>
+            </div>
+            <div className={styles.container}>
+                <p className={styles.p1}>High School of Dundee (2016/18)</p>
+                <p className={styles.p2}>SQA Highers/Adv Highers</p>
+            </div>
+            <div className={styles.container}>
+                <p className={styles.p1}>Lycée Piere de Coubertin (2011/16)</p>
+                <p className={styles.p2}>Collège/Lycée</p>
             </div>
             <h2>Experience</h2>
-            <div className={styles.jobcontainer}>
+            <div className={styles.container}>
                 <p className={styles.p1}>Yookr B.V. (2023)</p>
                 <p className={styles.p2}>Software / Technical Operations Engineer</p>
             </div>
-            <div className={styles.jobcontainer}>
+            <div className={styles.container}>
                 <p className={styles.p1}>Yookr B.V. (Summer 2023)</p>
                 <p className={styles.p2}>Software / Technical Operations Engineer Intern</p>
             </div>
-            <div className={styles.jobcontainer}>
+            <div className={styles.container}>
                 <p className={styles.p1}>Clevermed Ltd (2021/22)</p>
                 <p className={styles.p2}>Junior Software Engineer</p>
             </div>
-            <div className={styles.jobcontainer}>
+            <div className={styles.container}>
                 <p className={styles.p1}>Clevermed Ltd (Summer 2020)</p>
                 <p className={styles.p2}>Software Engineer Intern</p>
             </div>
