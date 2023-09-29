1c6a021ee80c15eec909289fc9f0652c3f239de7
JBLGithub <jordi.laird@gmail.com>
2023-09-29 19:10:39 +0100

    welcome

--- frontend/src/components/welcome/Welcome.tsx
+++ frontend/src/components/welcome/Welcome.tsx
@@ -1,11 +1,20 @@
 import styles from './Welcome.module.scss';
+import ReactTyped from "react-typed";
 
 const Welcome = () => {
     return (
         <div className={styles.welcome}>
-            <div>
-                <h1 className={styles.typewriter}>hello world...</h1>
-            </div>
+            <h1>
+                {""}
+                <ReactTyped
+                    strings={["Hello World!", "Welcome to my portfolio.", "Please get in touch..."]}
+                    typeSpeed={60}
+                    loop
+                    backSpeed={30}
+                    cursorChar="|"
+                    showCursor={true}
+                />
+            </h1>
         </div>
     )
 }
