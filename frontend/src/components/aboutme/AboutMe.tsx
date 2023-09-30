1a43a9a9205b8a7105dfe45563faec9fc27d34a1
JBLGithub <jordi.laird@gmail.com>
2023-09-30 15:52:52 +0100

    responsiveness

--- frontend/src/components/aboutme/AboutMe.tsx
+++ frontend/src/components/aboutme/AboutMe.tsx
@@ -4,39 +4,45 @@ const AboutMe = () => {
 
     return (
         <div className={styles.aboutme} id="me">
-            <h2>Introduction</h2>
+            <div className={styles.aboutMeItem}>
+                <h2>Introduction</h2>
                 <p>I am a Computer Science student pursuing an Integrated Masters program at the University of St. Andrews.
                 I am consistently seeking opportunities for knowledge enhancement through collaborative efforts in both professional work environments, academic pursuits, and personal projects.
                 Some of my activities include skiing, sailing, scuba diving, golf, frisby golf, cycling and when I'm not in the wilderness, I'm learning about networking and security.</p>
-            <h2>Education</h2>
-            <div className={styles.container}>
-                <p className={styles.p1}>University of St Andrews (2024)</p>
-                <p className={styles.p2}>Msci Computer Science</p>
             </div>
-            <div className={styles.container}>
-                <p className={styles.p1}>High School of Dundee (2016/18)</p>
-                <p className={styles.p2}>SQA Highers/Adv Highers</p>
+            <div className={styles.aboutMeItem}>
+                <h2>Education</h2>
+                <div className={styles.container}>
+                    <p className={styles.p1}>University of St Andrews (2024)</p>
+                    <p className={styles.p2}>Msci Computer Science</p>
+                </div>
+                <div className={styles.container}>
+                    <p className={styles.p1}>High School of Dundee (2016/18)</p>
+                    <p className={styles.p2}>SQA Highers/Adv Highers</p>
+                </div>
+                <div className={styles.container}>
+                    <p className={styles.p1}>Lycée Piere de Coubertin (2011/16)</p>
+                    <p className={styles.p2}>Collège/Lycée</p>
+                </div>
             </div>
-            <div className={styles.container}>
-                <p className={styles.p1}>Lycée Piere de Coubertin (2011/16)</p>
-                <p className={styles.p2}>Collège/Lycée</p>
-            </div>
-            <h2>Experience</h2>
-            <div className={styles.container}>
-                <p className={styles.p1}>Yookr B.V. (2023)</p>
-                <p className={styles.p2}>Software / Technical Operations Engineer</p>
-            </div>
-            <div className={styles.container}>
-                <p className={styles.p1}>Yookr B.V. (Summer 2023)</p>
-                <p className={styles.p2}>Software / Technical Operations Engineer Intern</p>
-            </div>
-            <div className={styles.container}>
-                <p className={styles.p1}>Clevermed Ltd (2021/22)</p>
-                <p className={styles.p2}>Junior Software Engineer</p>
-            </div>
-            <div className={styles.container}>
-                <p className={styles.p1}>Clevermed Ltd (Summer 2020)</p>
-                <p className={styles.p2}>Software Engineer Intern</p>
+            <div className={styles.aboutMeItem}>
+                <h2>Experience</h2>
+                <div className={styles.container}>
+                    <p className={styles.p1}>Yookr B.V. (2023)</p>
+                    <p className={styles.p2}>Software / Technical Operations Engineer</p>
+                </div>
+                <div className={styles.container}>
+                    <p className={styles.p1}>Yookr B.V. (Summer 2023)</p>
+                    <p className={styles.p2}>Software / Technical Operations Engineer Intern</p>
+                </div>
+                <div className={styles.container}>
+                    <p className={styles.p1}>Clevermed Ltd (2021/22)</p>
+                    <p className={styles.p2}>Junior Software Engineer</p>
+                </div>
+                <div className={styles.container}>
+                    <p className={styles.p1}>Clevermed Ltd (Summer 2020)</p>
+                    <p className={styles.p2}>Software Engineer Intern</p>
+                </div>
             </div>
         </div>
     );
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -2,8 +2,11 @@
 import { HashLink as Link } from 'react-router-hash-link';
 import styles from './Header.module.scss';
 import logo from '../../assets/logo.png';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [menuBarShowing, setMenuBarShowing] = useState(false);
 
     // offset fixed header - source : https://github.com/rafgraph/react-router-hash-link/issues/25#issuecomment-536688104
     const scrollWithOffset = (el:any) => {
@@ -12,31 +15,59 @@ const Header = () => {
         window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
     }
 
+    const handleMenuBarPressed = () => {
+        const links = document.getElementById('links');
+        const screenWidth = window.innerWidth;
+        if (screenWidth <= 650) {
+            if (!menuBarShowing && links) {
+                links.style.display = "flex";
+                setMenuBarShowing(true);
+            } else if (menuBarShowing && links) {
+                links.style.display = "none"; // or another appropriate value to hide the menu bar
+                setMenuBarShowing(false);
+            }
+        }
+    }
+
     return (
         <div className={styles.headercontainer}>
             <div className={styles.header}>
                 <div className={styles.logo}>
                     <img src={logo} alt='logo' />
                 </div>
-                <div className={styles.links}>
+                <div 
+                    className={styles.menuBarContainer}
+                    onClick={handleMenuBarPressed}
+                >
+                    <button className={styles.menuBar}>
+                            <hr></hr>
+                            <hr></hr>
+                            <hr></hr>
+                    </button>
+                </div>
+                <div id='links' className={styles.links}>
                     <Link
                         to="/#top"
                         className={styles.link}
+                        onClick={handleMenuBarPressed}
                     >home</Link>
                     <Link
                         to="/#me"
                         className={styles.link}
                         scroll={el => scrollWithOffset(el)}
+                        onClick={handleMenuBarPressed}
                     >me</Link>
                     <Link
                         to="/#technology"
                         className={styles.link}
                         scroll={el => scrollWithOffset(el)}
+                        onClick={handleMenuBarPressed}
                     >technology</Link>
                     <Link
                         to="/#project"
                         className={styles.link}
                         scroll={el => scrollWithOffset(el)}
+                        onClick={handleMenuBarPressed}
                     >projects</Link>
                 </div>
             </div>
